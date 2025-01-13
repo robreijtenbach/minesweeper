0856ed1ddb6a9414b2def0a848e9ffb2dd4a6801
robreijtenbach <33518020+robreijtenbach@users.noreply.github.com>
2025-01-13 20:37:14 +0100

    get_board_info now return board borders instead of corners and a list of all cell coords. Tried basic clicking functionality.

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def get_board_info(image):
 
     Args: image: input screenshot
     
-    Returns: dictionary with board size in cells and coords of corners.
+    Returns: dictionary with board size in cells, board border locations and list of all cells (coords).
     """
     board_corners = {}
 
@@ -98,7 +98,28 @@ def get_board_info(image):
     
     board_info = {}
     board_info["size"] = (horizontal_cells, vertical_cells)
-    board_info["corners"] = board_corners
+    board_info["left"] = board_corners["top_left"][0]
+    board_info["top"] = board_corners["top_left"][1]
+    board_info["right"] = board_corners["bottom_right"][0]
+    board_info["bottom"] = board_corners["bottom_right"][1]
+
+    # should be equal but might get rounding error
+    x_step = (board_info["right"] - board_info["left"]) / horizontal_cells
+    y_step = (board_info["bottom"] - board_info["top"]) / vertical_cells
+
+    cells = []
+    y = board_info["top"] + y_step/2
+    for i in range(vertical_cells): # TODO check for off by one
+        row = []
+        x = board_info["left"] + x_step/2
+        for j in range(horizontal_cells): # TODO check for off by one
+            row.append((x,y))
+            x += x_step
+        cells.append(row)
+        y += y_step
+    
+    board_info["cells"] = cells
+
     return board_info
 
 for w in enum_windows():
@@ -116,10 +137,21 @@ win32gui.SetForegroundWindow(window_id)
 l,t,r,b = win32gui.GetWindowRect(window_id)
 screenshot = pyautogui.screenshot(region=(l,t,r-l,b-t)).convert("RGBA")
 
+print(l,t,r,b)
+
 board_info = get_board_info(screenshot)
 
-for cc in board_info["corners"].values():
-    screenshot.putpixel(cc, COLOR_RED)
+#x,y = board_info["cells"][0][0]
+for x,y in board_info["cells"][0]:
+    x+=l
+    y+=t
+    pyautogui.moveTo(x, y, 1, pyautogui.easeInBounce)
+    #pyautogui.moveTo(x,y)
+    pyautogui.click()
+    #pyautogui.click(clicks=2, interval=0.25)
+
+#for cc in board_info["corners"].values():
+#    screenshot.putpixel(cc, COLOR_RED)
 
 print(screenshot.size)
 screenshot.save("test.png")
